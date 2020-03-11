3499e3cffabaec6523277d1afa469a922e40a3db
Ciiile <61187405+Ciiile@users.noreply.github.com>
2020-03-11 11:08:57 +0100

    Add files via upload
    Test 2.0

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace iPole_01
+{
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Cecilie", 21);
+
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
